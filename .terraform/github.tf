2eac6541330a06cbbbbf782da992476f595b5a82
Pedro Belem <pedrocb_c-s@hotmail.com>
2022-08-10 22:31:40 +0100

    Add github thumbprint

--- .terraform/github.tf
+++ .terraform/github.tf
@@ -5,7 +5,9 @@ resource "aws_iam_openid_connect_provider" "github" {
   client_id_list = [
     "sts.amazonaws.com"
   ]
-  thumbprint_list = []
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
 }
 
 resource "aws_iam_role" "github_actions" {
